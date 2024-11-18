18a11395cbd9ae6f2334fa3673429ab53174077d
wuerion <almerux@gmail.com>
2024-11-18 17:22:57 -0600

    Update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,7 @@ export default {
 				'spin-slow': 'spin 4s linear infinite'
 			},
 			colors : {
+				'body': 'linear-gradient(180deg, 233A4E, #9C6CB0)',
 				'buttonLink': 'linear-gradient(0deg, black, rgb(170, 170, 170))',
 			},
 		},
