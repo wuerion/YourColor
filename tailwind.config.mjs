048ac2428598ab85e908df7455b96be5567c2ee0
wuerion <almerux@gmail.com>
2025-01-17 19:34:53 -0600

    simplificar la configuración de fuentes en tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				'lato': ['Lato', 'sans-serif'],
-				'inconsolata': ['Inconsolata', 'monospace'],
+					'lato': ['Lato'],
+					'inconsolata': ['Inconsolata', 'monospace'],
 			},
 			animation: {
 				'spin-slow': 'spin 4s linear infinite'
