8a03f515f26915bbd007e91eaa8a1282e2884269
wuerion <almerux@gmail.com>
2025-11-15 20:18:24 -0600

    feat: adding more rules of seasons

--- src/logic/rules.js
+++ src/logic/rules.js
@@ -1,13 +1,32 @@
 //* Funciones para determiar la estacion
 
-import { spring } from "../data/seasons.js";
+import { spring, summer } from "../data/seasons.js";
 
 const RULES = [
+  //* Start Spring
+  {
+    name: "Primavera (Spring)",
+    match: (r) =>
+      r.tono_piel === "Muy Clara" ||
+      (((r.tono_piel === "Clara" && r.subTono_piel === "Calido") ||
+        r.subTono_piel === "Frio") &&
+        [
+          "Rubio Claro",
+          "Castaño Claro",
+          "Rubio Dorado",
+          "Rubio Miel",
+          "Caoba",
+        ].includes(r.color_cabello) &&
+        ["Azul", "Verde", "Marron Claro", "Marron Avellana"].includes(
+          r.color_ojos
+        )),
+    result: spring,
+  },
   {
     name: "Pirmavera Clara (Ligth Spring)",
     match: (r) =>
       r.tono_piel === "Clara" &&
-      r.subtono_piel === "Calido" &&
+      r.subTono_piel === "Calido" &&
       (r.color_cabello === "Rubio Miel" ||
         r.color_cabello === "Rubio Dorado") &&
       (r.color_ojos === "Azul Brillante" || r.color_ojos === "Verde Claro"),
@@ -25,6 +44,34 @@ const RULES = [
       (r.color_ojos === "Marron Avellana" &&
         r.destellos === "Dorados o Calidos"),
     result: spring,
+  },
+  {
+    name: "Primavera Brillante (Brigth Spring)",
+    match: (r) =>
+      (r.tono_piel === "Media" ||
+        (r.tono_piel === "Clara" &&
+          r.subTono_piel === "Calido" &&
+          r.color_cabello === "Rubio Dorado") ||
+        r.color_cabello === "Rubio Miel") &&
+      (r.color_ojos === "Azul" ||
+        r.color_ojos === "Verde" ||
+        r.color_ojos === "Marron Claro"),
+    result: spring,
+  },
+  //* End Spring
+  //* Start Summer
+  {
+    name: "Verano (Summer)",
+    match: (r) =>
+      r.tono_piel === "Muy Clara" &&
+      (r.tono_piel === "Medio" || r.subTono_piel === "Frio") &&
+      (r.color_cabello === "Rubio Claro" ||
+        r.color_cabello === "Rubio Cenizo" ||
+        (r.color_cabello === "Castaño Claro" && r.subTono_piel === "Frio")) &&
+      ["Azul", "Gris", "Verde", "Marron", "Marron Avellana"].includes(
+        r.color_ojos
+      ),
+    result: summer,
   },
 ];
 
