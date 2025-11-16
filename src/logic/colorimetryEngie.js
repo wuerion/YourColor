269266061a612317816570bbb1a6b52a61140334
wuerion <almerux@gmail.com>
2025-11-15 20:16:18 -0600

    feat: adding name upon retruning

--- src/logic/colorimetryEngie.js
+++ src/logic/colorimetryEngie.js
@@ -3,7 +3,7 @@ import RULES from "./rules";
 export default function calculateResult(answer) {
   for (const rule of RULES) {
     if (rule.match(answer)) {
-      return rule.result;
+      return rule.result && rule.name;
     }
   }
   return null;
