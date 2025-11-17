68f13f46236b831b4fbf8ac3e614a99bc16ffadc
wuerion <almerux@gmail.com>
2025-11-17 11:15:48 -0600

    feat: call function visibleCard to  functionUI

--- src/logic/colorimetryEngie.js
+++ src/logic/colorimetryEngie.js
@@ -1,8 +1,10 @@
 import RULES from "./rules";
+import visibleCard from "./functionsUi";
 
 export default function calculateResult(answer) {
   for (const rule of RULES) {
     if (rule.match(answer)) {
+      visibleCard(rule.name);
       return rule.result && rule.name;
     }
   }
