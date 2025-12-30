6ecb7ea519b0cb1e9a499d8fc7549a8d84f010a7
wuerion <almerux@gmail.com>
2025-12-30 15:56:30 -0600

    chore: removed old config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,37 @@
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-					'lato': ['Lato'],
-					'inconsolata': ['Inconsolata', 'monospace'],
-			},
-			animation: {
-				'spin-slow': 'spin 4s linear infinite'
-			},
-			colors : {
-				'body': 'linear-gradient(180deg, 233A4E, #9C6CB0)',
-				'buttonLink': 'linear-gradient(0deg, black, rgb(170, 170, 170))',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        julius: ["Julius Sans One"],
+        k2d: ["K2D"],
+        lato: ["Lato"],
+      },
+      colors: {
+        body: "#FAF8F0",
+      },
+      keyframes: {
+        imgAnimation: {
+          "0%": {
+            opacity: 1,
+          },
+          "45%": {
+            opacity: 1,
+          },
+          "55%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 0,
+          },
+        },
+      },
+      animation: {
+        // nombre: "keyframes duración timing-function iteración"
+        imgAnimate: "imgAnimation 3s linear infinite",
+        "spin-slow": "spin 4s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
