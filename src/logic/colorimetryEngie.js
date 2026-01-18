667c2cf6a9b461bcea00c1df7af29c079a568465
wuerion <almerux@gmail.com>
2026-01-17 19:51:39 -0600

    feat: new logic

--- src/logic/colorimetryEngie.js
+++ src/logic/colorimetryEngie.js
@@ -1,11 +1,15 @@
 import RULES from "./rules";
-import visibleCard from "./functionsUi";
+import { determineTheSubTone } from "./helpers";
 
 export default function calculateResult(answer) {
+  const fullAnswer = {
+    ...answer,
+    subTono_piel: determineTheSubTone(answer),
+  };
+
   for (const rule of RULES) {
-    if (rule.match(answer)) {
-      visibleCard(rule.name);
-      return rule.result && rule.name;
+    if (rule.match(fullAnswer)) {
+      return { name: rule.name, palette: rule.result };
     }
   }
   return null;
