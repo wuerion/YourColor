fb1d81bc10a22846a988fbc18aa37a2fdfd1628d
wuerion <almerux@gmail.com>
2026-02-08 13:00:09 -0600

    feat: creation the component card

--- src/components/test/Card.jsx
+++ src/components/test/Card.jsx
@@ -1,38 +1,129 @@
-import React from 'react'
-export default function Card({title, name, id, circle1, circle2, circle3, image, contCircle, onNext, children}) {
+import React, { useState, useEffect } from "react";
+export default function Card({
+  title,
+  name,
+  id,
+  circle1,
+  circle2,
+  circle3,
+  contCircle,
+  image,
+  onNext,
+  children,
+}) {
   const images = {
-      muyClara: {
-        component: '/tonoDePiel/muyClara.webp',
-        alt: 'Mujer con piel muy clara',
-      },
-      clara: {
-        component: '/tonoDePiel/clara.webp',
-        alt: 'Mujer con piel clara',
-      },
-      media: {
-        component: '/tonoDePiel/media.webp',
-        alt: 'Mujer con piel media/morena',
-      },
-      oscura: {
-        component: '/tonoDePiel/oscura.webp',
-        alt: 'Mujer con piel oscura',
-      },
-  }
+    "Muy Clara": {
+      component: "/tonoDePiel/muyClara.webp",
+      alt: "Mujer con piel muy clara",
+    },
+    Clara: {
+      component: "/tonoDePiel/clara.webp",
+      alt: "Mujer con piel clara",
+    },
+    Media: {
+      component: "/tonoDePiel/media.webp",
+      alt: "Mujer con piel media/morena",
+    },
+    Oscura: {
+      component: "/tonoDePiel/oscura.webp",
+      alt: "Mujer con piel oscura",
+    },
+    Negro: {
+      component: "/colorDeCabello/negro.webp",
+      alt: "Mujer con cabello de color negro",
+    },
+    "Castaño Oscuro": {
+      component: "/colorDeCabello/castañoOscuro.webp",
+      alt: "Mujer con cabello de color castaño oscuro",
+    },
+    "Castaño Claro / Medio": {
+      component: "/colorDeCabello/castañoClaro.webp",
+      alt: "Mujer con cabello de color castaño claro",
+    },
+    Rubio: {
+      component: "/colorDeCabello/rubio.webp",
+      alt: "Mujer con cabello de color rubio",
+    },
+    Pelirojo: {
+      component: "/colorDeCabello/pelirojo.webp",
+      alt: "Mujer con cabello de color pelirojo",
+    },
+    "Gris / Blanco": {
+      component: "/colorDeCabello/gris.webp",
+      alt: "Mujer con cabello blanco/gris",
+    },
+    "Azul Brillante": {
+      component: "/colorDeOjos/azulBrillante.webp",
+      alt: "Iris de color azul brillante",
+    },
+    "Azul Claro": {
+      component: "/colorDeOjos/azulClaro.webp",
+      alt: "Iris de color azul claro",
+    },
+    "Marron Avellana": {
+      component: "/colorDeOjos/marronAvellana.webp",
+      alt: "Iris de color marron avellana",
+    },
+    "Marron Oscuro": {
+      component: "/colorDeOjos/marronOscuro.webp",
+      alt: "Iris de color marron oscuro",
+    },
+    Verde: {
+      component: "/colorDeOjos/verde.webp",
+      alt: "Iris de color verde",
+    },
+    Gris: {
+      component: "/colorDeOjos/gris.webp",
+      alt: "Iris de color gris",
+    },
+    "Dorados / Calidos": {
+      component: "/destellos/dorados.webp",
+      alt: "Mujer con destellos/reflejos dorados o calidos",
+    },
+    "Plateados / Frios": {
+      component: "/destellos/plateados.webp",
+      alt: "Mujer con destellos/reflejos plateados o frios",
+    },
+  };
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
-      <p className='text-lg lg:text-xl'>{title}</p>
-        <label title={title} className='overflow-hidden rounded-lg group relative hover:cursor-pointer w-full h-full '>
-          <input type="radio" name={name} id={id} value={title} onClick={() => onNext(id)} className='hidden'/>
-          {children}
-          <div className={`flex gap-4 justify-center items-center w-full h-full ${contCircle} group-hover:scale-105`}>
-            <div className={`${circle1} w-28 h-28 lg:w-36 lg:h-36 rounded-full shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}></div>
-            <div className={`${circle2} w-28 h-28 lg:w-36 lg:h-36 rounded-full shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}></div>
-            <div className={`${circle3} w-28 h-28 lg:w-36 lg:h-36 rounded-full shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}></div>
-          </div>
-          <div className={`shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)] min-w-[100px] min-h-[200px] w-full h-full`}>
-          <img src={images[id].component} alt={images[id].alt} className={`absolute -z-10 w-full h-full transition-all delay-75 group-hover:scale-105 ${image}`}/>
-          </div>
-        </label>
+    <div className="flex flex-col justify-center items-center gap-5 border-2 p-2 border-natural-300 rounded lg:border-none">
+      <p className="text-lg lg:text-xl">{title}</p>
+      <label
+        title={title}
+        className="overflow-hidden rounded-lg group relative hover:cursor-pointer w-full h-full "
+      >
+        <input
+          type="radio"
+          name={name}
+          id={id}
+          value={title}
+          onClick={() => onNext(id)}
+          className="hidden"
+        />
+        <div
+          className={`flex gap-2 justify-center items-center w-full h-full ${contCircle} group-hover:scale-105`}
+        >
+          <div
+            className={`${circle1} w-28 h-28 lg:w-32 lg:h-32 rounded-full border-2 border-neutral-300 shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}
+          ></div>
+          <div
+            className={`${circle2} w-28 h-28 lg:w-32 lg:h-32 rounded-full border-2 border-neutral-300 shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}
+          ></div>
+          <div
+            className={`${circle3} w-28 h-28 lg:w-32 lg:h-32 rounded-full border-2 border-neutral-300 shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)]`}
+          ></div>
+        </div>
+        <div
+          className={`shadow-[inset_0_0_24px_8px_rgba(0,0,0,40%)] min-w-[100px] min-h-[200px] w-full h-full`}
+        >
+          <img
+            src={images[title] ? images[title].component : ""}
+            alt={images[title] ? images[title].alt : ""}
+            className={`absolute -z-10 w-full h-full transition-all delay-75 group-hover:scale-105 ${image}`}
+          />
+        </div>
+      </label>
+      {children}
     </div>
-  )
+  );
 }
