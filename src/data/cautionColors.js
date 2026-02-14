24d34b2cbd0c8ed5e37e1ee93a9232f238532681
wuerion <almerux@gmail.com>
2026-02-13 19:29:34 -0600

    feat: data caution colors

---
+++ src/data/cautionColors.js
@@ -0,0 +1,96 @@
+export const springLight = [
+  { nombre: "Negro", hexa: "#000000" },
+  { nombre: "Borgoña", hexa: "#800020" },
+  { nombre: "Berenjena", hexa: "#4B0082" },
+  { nombre: "Azul Marino Frio", hexa: "#000080" },
+  { nombre: "Gris Carbón", hexa: "#37454GF" },
+  { nombre: "Rosa Viejo Frio", hexa: "#C08081" },
+];
+export const springTrue = [
+  { nombre: "Blanco Puro Frio", hexa: "#FFFFFF" },
+  { nombre: "Cyan Icy", hexa: "#00FFFF" },
+  { nombre: "Fucsia Frio", hexa: "#FF00FF" },
+  { nombre: "Gris Medio", hexa: "#808080" },
+  { nombre: "Azul Real Frio", hexa: "#4169E1" },
+  { nombre: "Lavanda Frio", hexa: "#E6E6FA" },
+];
+export const springBright = [
+  { nombre: "Gris Ratón", hexa: "#6D7B8D" },
+  { nombre: "Beige Sucio", hexa: "#967117" },
+  { nombre: "Rosa polvo", hexa: "#D8B2BE" },
+  { nombre: "Verde Salvia Apagado", hexa: "#9DC183" },
+  { nombre: "Azul Pizarra", hexa: "#708090" },
+  { nombre: "Marrón Topo", hexa: "#483C32" },
+];
+export const summerLight = [
+  { nombre: "Negro", hexa: "#000000" },
+  { nombre: "Naranja Vivo", hexa: "#FF5733" },
+  { nombre: "Marrón Chocolate Cálido", hexa: "#D2691E" },
+  { nombre: "Amarillo Mostaza", hexa: "#FFDB58" },
+  { nombre: "Rojo Tomate", hexa: "#FF6347" },
+  { nombre: "Verde Lima Cálido", hexa: "#32CD32" },
+];
+export const summerTrue = [
+  { nombre: "Naranja Calabaza", hexa: "#FF7518" },
+  { nombre: "Amarillo Oro", hexa: "#FFD700" },
+  { nombre: "Marrón Óxido", hexa: "#B7410E" },
+  { nombre: "Verde Oliva Cálido", hexa: "#808000" },
+  { nombre: "Salmón Cálido", hexa: "#FF8C69" },
+  { nombre: "Terracota", hexa: "#E2725B" },
+];
+export const summerSoft = [
+  { nombre: "Negro Puro", hexa: "#000000" },
+  { nombre: "Blanco Óptico", hexa: "#FFFFFF" },
+  { nombre: "Rosa Neón", hexa: "#FF1493" },
+  { nombre: "Verde Neón", hexa: "#39FF14" },
+  { nombre: "Azul Eléctrico", hexa: "#0000FF" },
+  { nombre: "Amarillo Resaltador", hexa: "#FFFF00" },
+];
+export const autumnSoft = [
+  { nombre: "Negro", hexa: "#000000" },
+  { nombre: "Blanco Puro", hexa: "#FFFFFF" },
+  { nombre: "Fucsia Brillante", hexa: "#FF00FF" },
+  { nombre: "Azul Real Intenso", hexa: "#002366" },
+  { nombre: "Violeta Brillante", hexa: "#8A2BE2" },
+  { nombre: "Gris Hielo", hexa: "#8A2BE2" },
+];
+export const autumnTrue = [
+  { nombre: "Rosa Barbie Frío", hexa: "#E0218A" },
+  { nombre: "Azul Cielo Frío", hexa: "#87CEEB" },
+  { nombre: "Gris Azulado Frío", hexa: "#A9A9A9" },
+  { nombre: "Lavanda", hexa: "#E6E6FA" },
+  { nombre: "Aguamarina Frío", hexa: "#7FFFD4" },
+  { nombre: "Blanco Azulado", hexa: "#F0F8FF" },
+];
+export const autumnDeep = [
+  { nombre: "Rosa Pastel Claro", hexa: "#FFD1DC" },
+  { nombre: "Rosa Bebé Helado", hexa: "#E0FFFF" },
+  { nombre: "Amarillo Pálido Frío", hexa: "#FFFFE0" },
+  { nombre: "Menta Claro", hexa: "#98FF98" },
+  { nombre: "Blanco Tiza", hexa: "#F5F5F5" },
+  { nombre: "Gris Claro Frío", hexa: "#DCDCDC" },
+];
+export const winterDeep = [
+  { nombre: "Mostaza", hexa: "#FFDB58" },
+  { nombre: "Naranja Quemado", hexa: "#CC5500" },
+  { nombre: "Marrón Topo Cálido", hexa: "#483C32" },
+  { nombre: "Verde musgo", hexa: "#8A9A5B" },
+  { nombre: "Melocotón Suave", hexa: "#FFDAB9" },
+  { nombre: "Beige Cálido", hexa: "#F5F5DC" },
+];
+export const winterTrue = [
+  { nombre: "Naranja Puro", hexa: "#FFA500" },
+  { nombre: "Amarillo Yema", hexa: "#FFAE42" },
+  { nombre: "Marrón Ladrillo", hexa: "#CB4154" },
+  { nombre: "Rojo Tomate Cálido", hexa: "#FF6347" },
+  { nombre: "Verde Lima Cálido", hexa: "#32CD32" },
+  { nombre: "Crema/Marfil Cálido", hexa: "#FFFFF0" },
+];
+export const winterBright = [
+  { nombre: "Gris Medio Apagado", hexa: "#808080" },
+  { nombre: "Rosa Palo Apagado", hexa: "#C4AEAD" },
+  { nombre: "Beige Neutro Apagado", hexa: "#A89F91" },
+  { nombre: "Verde Militar Deslavado", hexa: "#78866B" },
+  { nombre: "Azul Grisáceo", hexa: "#6699CC" },
+  { nombre: "Marrón Café Suave", hexa: "#8B4513" },
+];
