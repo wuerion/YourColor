9157f39d24981994a27522be184791f2c869aa9d
wuerion <almerux@gmail.com>
2026-02-13 19:32:29 -0600

    feat:a new property was added to the object

--- src/logic/colorimetryEngie.js
+++ src/logic/colorimetryEngie.js
@@ -3,12 +3,18 @@ import { determineTheSubTone } from "./helpers";
 
 export function calculateResult(answer) {
   const fullAnswers = {
-    ...answer, subTono_piel: determineTheSubTone(answer)
-  }
-  
+    ...answer,
+    subTono_piel: determineTheSubTone(answer),
+  };
+
   for (const rule of RULES) {
     if (rule.match(fullAnswers)) {
-      return {name: rule.name, palette: rule.result}
+      return {
+        name: rule.name,
+        palette: rule.result,
+        paletteCaution: rule.cautionResult,
+        textResult: rule.textResult,
+      };
     }
   }
   return null;
