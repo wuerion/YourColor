355b59226a3cba39f2ad183b0aaa0867cdfb3a14
wuerion <almerux@gmail.com>
2026-03-08 16:40:53 -0600

    feat: remove unnecessary code

--- src/components/test/comparison.jsx
+++ src/components/test/comparison.jsx
@@ -47,60 +47,8 @@ function comparison({ setIsVisible, data, image }) {
     setSelecctNameRight(name);
   };
 
-  function GridColors({
-    data,
-    rotated,
-    position,
-    selecctColor,
-    selecctName,
-    handleClick,
-    colorText,
-    rounded,
-    text,
-  }) {
-    return (
-      <div className={`flex flex-col ${position} w-full h-full gap-3`}>
-        <p className="text-2xl block lg:hidden text-center">{text}</p>
-        <div
-          className={`flex lg:grid lg:grid-cols-1 lg:max-w-[200px] gap-x-1 lg:gap-y-2 p-1 justify-items-center w-full h-full transition-all delay-200 `}
-        >
-          <p className="text-lg text-center hidden lg:block">{text}</p>
-          {data.slice(0, 6).map((index, key) => (
-            <div
-              id={index.nombre}
-              key={key}
-              className=" w-full h-full flex flex-col justify-center items-center"
-            >
-              <div
-                className={`w-[45px] h-[42px] lg:w-[80px] lg:h-[80px] rounded-lg flex items-center justify-center shadow-[2px_2px_4px_rgba(0,0,0,0.40)] hover:cursor-pointer`}
-                title={index.nombre}
-                style={{ backgroundColor: index.hexa }}
-                onClick={(e) => {
-                  handleClick(e, index.hexa, index.nombre);
-                }}
-              ></div>
-            </div>
-          ))}
-        </div>
-        <div className="min-h-[100px] lg:h-full w-full min-w-[200px] lg:max-w-[260px]">
-          <div
-            className={`w-full h-full flex justify-center items-center shadow-[2px_2px_4px_rgba(0,0,0,0.40)] ${rounded}`}
-            style={{ backgroundColor: selecctColor }}
-          >
-            <p
-              className={`text-lg lg:text-2xl text-nowrap uppercase font-bold ${rotated}`}
-              style={{ color: colorText }}
-            >
-              {selecctName}
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <section className="relative z-50 lg:h-dvh">
+    <section className="relative z-50 h-dvh w-dvw">
       <div class="absolute w-full h-full bg-[#FAF8F0] overflow-hidden flex items-center justify-center -z-10">
         <div class="flex w-full h-full items-center justify-center space-x-[-40px]">
           <div class="w-32 md:w-80 h-full transform bg-[#FAF8F0] -skew-x-[15deg] shadow-[20px_0_10px_rgba(0,0,0,0.5)] z-[41] relative"></div>
@@ -123,43 +71,89 @@ function comparison({ setIsVisible, data, image }) {
         <h2 className="text-xl lg:text-4xl text-center font-bold">
           MODO COMPARACION
         </h2>
-        <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto">
-          <div className="h-full max-h-[600px]">
-            <GridColors
-              text={"Precaucion"}
-              data={data.paletteCaution}
-              rotated={"lg:-rotate-90"}
-              position={"lg:flex-row"}
-              rounded={
-                "rounded-tl-lg rounded-tr-lg lg:rounded-tr-none lg:rounded-tl-lg lg:rounded-bl-lg"
-              }
-              selecctColor={selecctColorLeft}
-              selecctName={selecctNameLeft}
-              handleClick={handleClickLeft}
-              colorText={calculateColor(selecctColorLeft)}
-            />
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-2">
+          {/* color palette top */}
+          <div className="w-full lg:w-fit">
+            <p className="text-center text-lg my-1">Precaucion</p>
+            <div className="flex lg:grid lg:grid-cols-1 gap-1 w-full ">
+              {data.paletteCaution.slice(0, 6).map((index, key) => (
+                <div
+                  id={index.nombre}
+                  key={key}
+                  className="w-full h-full flex flex-col justify-center items-center"
+                >
+                  <div
+                    className="w-full h-[42px] md:w-1/2 lg:w-[80px] lg:h-[80px] rounded-lg flex items-center justify-center shadow-[2px_2px_4px_rgba(0,0,0,0.40)] hover:cursor-pointer"
+                    title={index.nombre}
+                    style={{ backgroundColor: index.hexa }}
+                    onClick={(e) => {
+                      handleClickLeft(e, index.hexa, index.nombre);
+                    }}
+                  ></div>
+                </div>
+              ))}
+            </div>
           </div>
-          <div className="h-full max-h-[600px] shadow-[2px_2px_2px_rgba(0,0,0,0.40)]">
-            <img
-              src={images[image] ? images[image].component : ""}
-              alt={images[image] ? images[image].alt : ""}
-              className="w-full h-full"
-            />
+          {/* image center*/}
+          <div className="w-full h-full max-h-[600px] lg:w-fit grid grid-cols-[80px_2fr_80px] md:grid-cols-[200px_2fr_200px] lg:grid-cols-[200px_400px_200px] justify-items-stretch shadow-[2px_2px_2px_rgba(0,0,0,0.40)] rounded-lg">
+            {/* top color */}
+            <div className="h-full w-full lg:min-w-[200px] lg:max-w-[260px]">
+              <div
+                className="w-full h-full flex justify-center items-center rounded-tr-none rounded-tl-lg rounded-bl-lg"
+                style={{ backgroundColor: selecctColorLeft }}
+              >
+                <p
+                  className="text-base lg:text-2xl text-nowrap uppercase font-bold -rotate-90"
+                  style={{ color: calculateColor(selecctColorLeft) }}
+                >
+                  {selecctNameLeft}
+                </p>
+              </div>
+            </div>
+            {/* image */}
+            <div className="w-full h-full lg:max-h-[600px] ">
+              <img
+                src={images[image] ? images[image].component : ""}
+                alt={images[image] ? images[image].alt : ""}
+                className="w-full h-full"
+              />
+            </div>
+            {/* bottom color */}
+            <div className="h-full w-full lg:min-w-[200px] lg:max-w-[260px]">
+              <div
+                className="w-full h-full flex justify-center items-center rounded-bl-none rounded-tr-lg rounded-br-lg"
+                style={{ backgroundColor: selecctColorRight }}
+              >
+                <p
+                  className="text-base lg:text-2xl text-nowrap uppercase font-bold rotate-90"
+                  style={{ color: calculateColor(selecctColorRight) }}
+                >
+                  {selecctNameRight}
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="h-full max-h-[600px]">
-            <GridColors
-              text={"Favorecen"}
-              data={data.palette}
-              rotated={"lg:-rotate-90"}
-              position={"flex-col-reverse lg:flex-row-reverse"}
-              rounded={
-                "rounded-bl-lg rounded-br-lg lg:rounded-bl-none lg:rounded-tr-lg lg:rounded-br-lg"
-              }
-              selecctColor={selecctColorRight}
-              selecctName={selecctNameRight}
-              handleClick={handleClickRight}
-              colorText={calculateColor(selecctColorRight)}
-            />
+          {/* color palette bottom */}
+          <div className="w-full lg:w-fit">
+            <div className="flex lg:grid lg:grid-cols-1 gap-1 w-full">
+              {data.palette.slice(0, 6).map((index, key) => (
+                <div
+                  id={index.nombre}
+                  key={key}
+                  className="w-full h-full flex flex-col justify-center items-center"
+                >
+                  <div
+                    className="w-full h-[42px] md:w-1/2 lg:w-[80px] lg:h-[80px] rounded-lg flex items-center justify-center shadow-[2px_2px_4px_rgba(0,0,0,0.40)] hover:cursor-pointer"
+                    title={index.nombre}
+                    style={{ backgroundColor: index.hexa }}
+                    onClick={(e) => {
+                      handleClickRight(e, index.hexa, index.nombre);
+                    }}
+                  ></div>
+                </div>
+              ))}
+            </div>
+            <p className="text-center text-lg my-1">Favorecen</p>
           </div>
         </div>
       </div>
