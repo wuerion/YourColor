9242712a07a64eac89f3177c38b5986154759814
wuerion <almerux@gmail.com>
2026-03-15 16:25:13 -0600

    update?

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,19 @@
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-					'lato': ['Lato'],
-					'inconsolata': ['Inconsolata', 'monospace'],
-			},
-			animation: {
-				'spin-slow': 'spin 4s linear infinite'
-			},
-			colors : {
-				'body': 'linear-gradient(180deg, 233A4E, #9C6CB0)',
-				'buttonLink': 'linear-gradient(0deg, black, rgb(170, 170, 170))',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ["Lato"],
+        inconsolata: ["Inconsolata", "monospace"],
+      },
+      animation: {
+        "spin-slow": "spin 4s linear infinite",
+      },
+      colors: {
+        body: "linear-gradient(180deg, 233A4E, #9C6CB0)",
+        buttonLink: "linear-gradient(0deg, black, rgb(170, 170, 170))",
+      },
+    },
+  },
+  plugins: [],
+};
