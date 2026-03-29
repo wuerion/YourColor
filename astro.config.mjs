19feb4544f2ebe13efe89a9f070327b4f155230e
wuerion <almerux@gmail.com>
2026-03-28 19:37:24 -0600

    chore(package): add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import React from "@astrojs/react";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel(),
   integrations: [tailwind(), React()],
 });
