949f95e65628b484eaa57ef057409f9dd85e6308
wuerion <almerux@gmail.com>
2026-04-03 13:49:12 -0600

    chore(update): update the vercel /static to the standard version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import React from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
