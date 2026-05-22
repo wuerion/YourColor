fdff1fef14f25d95a37bc1711aa34c45ca151b06
wuerion <almerux@gmail.com>
2026-05-21 19:02:18 -0600

    feat: uncomment

--- src/components/test/result.jsx
+++ src/components/test/result.jsx
@@ -66,11 +66,11 @@ function result({ result, image, gander, apiUrl }) {
       console.log(text)
       
       try {
-      //   const response = await fetch(`${VITE_API_URL}/api/pexels/photos`, {
-      //     method: "POST",
-      //     headers: { "Content-Type": "application/json" },
-      //     body: JSON.stringify({ query: text }),
-      //   });
+        const response = await fetch(`${VITE_API_URL}/api/pexels/photos`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ query: text }),
+        });
 
         console.log(response)
 
