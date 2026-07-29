01f0e8f80a66197dad8141ede4680cced79d477d
emmanuel <almerux@gmail.com>
2026-07-28 19:04:11 -0600

    chore(sitemap): add site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import tailwind from "@astrojs/tailwind";
 import React from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  site: 'https://yourcolor.axvolestudio.com',
   adapter: vercel(),
-  integrations: [tailwind(), React()],
+  integrations: [tailwind(), React(), sitemap()],
 });
